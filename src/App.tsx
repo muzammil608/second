f65279dcae94837986f55639e1f2246d901ed91d
muzammil608 <muzmalabbas579@gmail.com>
2025-03-11 01:13:08 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import AddToDo from "./components/addtodo"
+import Navbar from "./components/navbar"
+import "./App.css"
+import Todos from "./components/todos"
+
+
+const App = () => {
+  return (
+   <main>
+      <h1>TODO REACT</h1>
+      <Navbar />
+      <AddToDo />
+      <Todos />
+   </main>
+  )
+}
+
+export default App
